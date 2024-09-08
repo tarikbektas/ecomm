088f4db8c90d275e94762def8b726f05d452310d
tarık bektas <127250165+tarikbektas@users.noreply.github.com>
2024-09-08 16:27:49 +0300

    proje dosya yüklenmesi

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./layout.css";
+ 
+const Header = () => {
+ 
+  return (
+    <div>
+    <div className="header-main">
+      <div className="logo-main">
+        <img
+          className="logo"
+          alt="logo"
+          src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=626&ext=jpg"
+        ></img>
+      </div>
+      <div className="header-left">
+        <div className="h-50">asdas</div>
+        <div className="header-left-bottom h-50"  >adasd</div>
+       
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Header;
